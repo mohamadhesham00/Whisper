197ab1754dc5efa14e082d07ae01e2c25db4f96b
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-12-04 17:52:38 +0200

    Feat: Implement unique data validation for user creation to prevent duplicate entries

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
 import { Model } from 'mongoose';
+import { ApiResponseUtil } from 'src/common/utils/api-response.util';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
@@ -13,6 +14,7 @@ export class UsersService {
   async create(createUserDto: CreateUserDto) {
     const salt = await bcrypt.genSalt(10);
     const hashed = await bcrypt.hash(createUserDto.password, salt);
+    await this.checkIfUniqueData(createUserDto); // throws if not unique
 
     const user = {
       fName: createUserDto.fName,
@@ -30,6 +32,36 @@ export class UsersService {
     return this.userModel.findOne({ email }).exec();
   }
 
+  async checkIfUniqueData({ email, userName, phone }: any) {
+    const existingUser = await this.userModel
+      .findOne({
+        $or: [{ email }, { userName }, { phone }],
+      })
+      .exec();
+    if (existingUser) {
+      if (existingUser.email === email)
+        return ApiResponseUtil.throwError(
+          'Email already exists',
+          'EMAIL_EXISTS',
+          HttpStatus.BAD_REQUEST,
+        );
+
+      if (existingUser.userName === userName)
+        return ApiResponseUtil.throwError(
+          'Username already exists',
+          'USERNAME_EXISTS',
+          HttpStatus.BAD_REQUEST,
+        );
+
+      if (existingUser.phone === phone)
+        return ApiResponseUtil.throwError(
+          'Phone number already exists',
+          'PHONE_EXISTS',
+          HttpStatus.BAD_REQUEST,
+        );
+    }
+    return true;
+  }
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
   }
