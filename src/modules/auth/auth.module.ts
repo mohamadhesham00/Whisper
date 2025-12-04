ab01afd79350b9aebe1d47d469d1bfa44b3600b1
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-12-04 18:11:35 +0200

    refactor(auth): remove unused LocalStrategy from AuthModule providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
@@ -17,6 +16,6 @@ import { LocalStrategy } from './local.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
